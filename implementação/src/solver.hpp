fd7ce88ae52caca4d99cf64ab5a489d5c46a0c3e
Pedro Lucas Damasceno <rseiken@hotmail.com>
2022-11-13 17:06:07 -0300

    Substituindo swap de colunas por ordenação

--- implementação/src/solver.hpp
+++ implementação/src/solver.hpp
@@ -44,7 +44,7 @@ void singleRun(string inputFileName, ofstream& outputFile, int run);
 
 int machines, tools, jobs, indexChangedTool; /* quantity of machines, tools and jobs. Index of the tool to be substituted */
 unsigned int fillJob, fillTool, switchCount, jobCount; /* current job and tool after initializing the magazine and switch and job counters */
-vector<vector<vector<int>>> npmJobSequency; /* current jobs and tools assigned to each machine */
+vector<vector<int>> npmJobSequency; /* current jobs and tools assigned to each machine */
 vector<vector<int>> toolsAndJobs; /* all tools and jobs */
 vector<vector<int>> npmJobTime; /* time cost of each job on each machine */
 vector<vector<int>> npmCurrentMagazines; /* current magazines of each machine */
@@ -60,11 +60,11 @@ void fillStartMagazine(int machineIndex) {
 
     fill(npmCurrentMagazines[machineIndex].begin(), npmCurrentMagazines[machineIndex].end(), 0);
     
-    for(unsigned int i = 0 ; i <= jobCount ; i++) {
+    for(unsigned int i = 0 ; i < jobCount ; i++) {
         fillJob = i;
         for (int j = 1 ; j <= tools ; j++) {
             fillTool = j;
-            if(npmJobSequency[machineIndex][j][i]) {
+            if(toolsAndJobs[j][npmJobSequency[machineIndex][i]]) {
                 if(aux < npmMagazineCapacity[machineIndex]) {
                     if (find(npmCurrentMagazines[machineIndex].begin(), npmCurrentMagazines[machineIndex].end(), j) == npmCurrentMagazines[machineIndex].end()) {
                         npmCurrentMagazines[machineIndex][aux++] = j;
@@ -75,7 +75,7 @@ void fillStartMagazine(int machineIndex) {
                 }
             }
         }
-    }
+    } 
     return;
 }
 
@@ -83,8 +83,7 @@ void KTNS(int machineIndex, int currentJob) {
     int aux = 0;
     
     for (auto i : npmCurrentMagazines[machineIndex]) {
-        (!npmToolsNeededSoonest[machineIndex][aux]) ? npmToolsNeededSoonest[machineIndex][aux] = toolsDistance(machineIndex, currentJob, i) : npmToolsNeededSoonest[machineIndex][aux]--;
-        aux++;
+        npmToolsNeededSoonest[machineIndex][aux++] = toolsDistance(machineIndex, currentJob, i);
     }
 }
 
@@ -92,55 +91,55 @@ int toolsDistance (int machineIndex, int currentJob, int currentTool) {
     int aux = 0;
 
     for (unsigned int i = currentJob ; i <= jobCount ; i++) {
-        if (!npmJobSequency[machineIndex][currentTool][i])
-            aux++;
-        else
+        if (toolsAndJobs[currentTool][i])
             return aux;
-    }
+        else
+            aux++;
+    } 
 
     return INT_MAX;
 }
 
 int toolSwitchesEvaluation(int machineIndex) {
-    jobCount = npmJobSequency[machineIndex][0].size();
+    jobCount = npmJobSequency[machineIndex].size();
     switchCount = 0;
 
     fillStartMagazine(machineIndex);
 
-    for (unsigned int i = fillJob ; i <= jobCount ; i++) {
+    for (unsigned int i = fillJob ; i < jobCount ; i++) {
         KTNS(machineIndex, i);
         for(int j = 1 ; j <= tools ; j++) {
-            if(npmJobSequency[machineIndex][j][i] && find(npmCurrentMagazines[machineIndex].begin(), npmCurrentMagazines[machineIndex].end(), j) == npmCurrentMagazines[machineIndex].end()) {         
+            if(toolsAndJobs[j][npmJobSequency[machineIndex][i]] && find(npmCurrentMagazines[machineIndex].begin(), npmCurrentMagazines[machineIndex].end(), j) == npmCurrentMagazines[machineIndex].end()) {         
                 indexChangedTool = distance(npmToolsNeededSoonest[machineIndex].begin(), max_element(npmToolsNeededSoonest[machineIndex].begin(), npmToolsNeededSoonest[machineIndex].end()));
                 npmCurrentMagazines[machineIndex][indexChangedTool] = j;
                 npmToolsNeededSoonest[machineIndex][indexChangedTool] = 0;
                 switchCount++;
             }
         }
-    }
+    } 
 
     return switchCount;
 }
 
 int flowtimeEvaluation(int machineIndex) {
-    jobCount = npmJobSequency[machineIndex][0].size();
+    jobCount = npmJobSequency[machineIndex].size();
     int flowTime = 0;
     
     fillStartMagazine(machineIndex);
     
-    for (unsigned int i = 0 ; i <= jobCount ; i++) {
+    for (unsigned int i = 0 ; i < jobCount ; i++) {
         KTNS(machineIndex, i);
         switchCount = 0;
         for (int j = 1 ; j <= tools ; j++) {
-            if(npmJobSequency[machineIndex][j][i] && find(npmCurrentMagazines[machineIndex].begin(), npmCurrentMagazines[machineIndex].end(), j) == npmCurrentMagazines[machineIndex].end()) {
+            if(toolsAndJobs[j][i] && find(npmCurrentMagazines[machineIndex].begin(), npmCurrentMagazines[machineIndex].end(), j) == npmCurrentMagazines[machineIndex].end()) {
                 int indexChangedTool = distance(npmToolsNeededSoonest[machineIndex].begin(), max_element(npmToolsNeededSoonest[machineIndex].begin(), npmToolsNeededSoonest[machineIndex].end()));
                 npmCurrentMagazines[machineIndex][indexChangedTool] = j;
                 npmToolsNeededSoonest[machineIndex][indexChangedTool] = 0;
                 switchCount++;
             }
         }
-        flowTime += npmJobTime[machineIndex][npmJobSequency[machineIndex][0][i]] + switchCount * npmSwitchCost[machineIndex];
-    }
+        flowTime += npmJobTime[machineIndex][i] + switchCount * npmSwitchCost[machineIndex];
+    } 
     
     return flowTime;
 }
@@ -148,11 +147,11 @@ int flowtimeEvaluation(int machineIndex) {
 int makespanEvaluation(int machineIndex) {
 	int makespan = 0;
 
-    for (unsigned int i = 0 ; i < npmJobSequency[machineIndex][0].size() ; i++) {
-        makespan += npmJobTime[machineIndex][npmJobSequency[machineIndex][0][i]];
+    for (unsigned int i = 0 ; i < npmJobSequency[machineIndex].size() ; i++) {
+        makespan += npmJobTime[machineIndex][npmJobSequency[machineIndex][i]];
     }
 
-    makespan += toolSwitchesEvaluation(machineIndex) * npmSwitchCost[machineIndex];
+    makespan += toolSwitchesEvaluation(machineIndex) * npmSwitchCost[machineIndex]; 
     
     return makespan;
 }
@@ -201,12 +200,11 @@ void readProblem(string fileName) {
             npmJobTime[i].push_back(aux);
         }
     }
+    
     for(int i = 1 ; i < tools + 1 ; i++) {
         for(int j = 0 ; j < jobs ; j++) {
             fpIn >> aux;
             toolsAndJobs[i].push_back(aux);
-            for (int l = 0 ; l < machines ; l++) /* Temp */
-                npmJobSequency[l][i].push_back(aux);
         }
     }
 }
@@ -220,10 +218,10 @@ void initialization() {
 
     npmJobTime.resize(machines);
 
-    npmJobSequency.resize(machines, vector<vector<int>>(tools + 1));
+    npmJobSequency.resize(machines);
     for(int i = 0 ; i < machines ; i++) { /* Atribuindo todos os j a todas as máquinas P/ TESTE */
         for(int j = 0 ; j < jobs ; j ++) {
-            npmJobSequency[i][0].push_back(j);
+            npmJobSequency[i].push_back(j);
         }
     }
 
@@ -245,9 +243,6 @@ void initialization() {
 
 void termination() {
     for (auto& v : npmJobSequency) {
-        for (auto& x : v) {
-            x.clear();
-        }
         v.clear();
     }
     for (auto& v : npmJobTime) {
@@ -291,9 +286,16 @@ void printSolution(string inputFileName, double runningTime, int run, S &s) {
     for (int i = 0 ; i < machines ; i++) {
         s << "- MACHINE [" << i << "]" << "\n\n";
 
-        if (PRINT_MATRIX) s << npmJobSequency[i];
+        if (PRINT_MATRIX) {
+            for (int j = 1 ; j <= tools ; j++) {
+                for (int k : npmJobSequency[i]) {
+                    s << toolsAndJobs[j][k] << " ";
+                }
+                s << endl;
+            }
+        }
 
-        s << "JOBS " << npmJobSequency[i][0];
+        s << "\nJOBS " << npmJobSequency[i];
 
         totalToolSwitches += npmCurrentToolSwitches[i] = toolSwitchesEvaluation(i);
         totalFlowtime += npmCurrentFlowTime[i] = flowtimeEvaluation(i);
